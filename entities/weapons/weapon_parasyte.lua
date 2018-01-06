c0dac3fe043480ae8009ea8d98844e725da5dc6d
Hoccka <35156831+Hoccka@users.noreply.github.com>
2018-01-06 23:13:08 +0000

    Added Parasyte SWEP for realsies

--- entities/weapons/weapon_parasyte.lua
+++ entities/weapons/weapon_parasyte.lua
@@ -5,7 +5,7 @@ SWEP.Instructions 	= "Left mouse to attack."
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 
-SWEP.Primary.Clipsize  = -1
+SWEP.Primary.ClipSize  = -1
 SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = "none"
@@ -15,13 +15,14 @@ SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
 
+SWEP.SetHoldType = "normal"
 SWEP.DrawAmmo = false
 
 SWEP.ViewModel = ""
 SWEP.WorldModel = ""
 
-SWEP.Slot = 2
-SWP.SlotPos = 1
+SWEP.Slot = 0
+SWEP.SlotPos = 1
 
  function SWEP:Initialize()
     self:SetHoldType("normal")
@@ -31,8 +32,69 @@ end
 function SWEP:Deploy()
 	self:GetOwner():DrawViewModel(false)
 	self:GetOwner():DrawViewModel(false)
+end
 
 function SWEP:Holster()
 	self:GetOwner():DrawViewModel(true)
 	return true
+end
+
+local SwingSound = "npc/zombie/claw_miss1.wav"
+local HitSound = "npc/zombie/claw_strike1.wav"
+
+function SWEP:PrimaryAttack()
+	if (CLIENT) then return end
+	
+	local ply = self:GetOwner()
+	
+	ply:LagCompensation(true)
+	
+	local shootpos = ply:GetShootPos()
+	local endshootpos = shootpos + ply:GetAimVector() * 100
+	local tmin = Vector(1, 1, 1) * -10
+	local tmax = Vector(1, 1, 1) * 10
+	
+	local tr = util.TraceHull({start = shootpos,
+		endpos = endshootpos,
+		filter = ply,
+		mask = MASK_SHOT_HULL,
+		mins = tmin,
+		maxs = tmax,})
+		
+	if (not IsValid(tr.Entity)) then
+		tr = util.TraceLine({start = shootpos,
+		endpos = endshootpos,
+		filter = ply,
+		mask = MASK_SHOT_HULL})
+	end
+	
+	local ent = tr.Entity
+	
+	if (IsValid(ent) and ent:IsPlayer()) then
+		sound.Play(HitSound, (ply:GetPos()))
+		ent:SetHealth(ent:Health() - 50)
+		if (ent:Health() < 1) then
+			ent:Kill()
+		end
+		
+	elseif (IsValid(ent) and ent:IsNPC()) then
+		sound.Play(HitSound, (ply:GetPos()))
+		ent:SetHealth(ent:Health() - 50)
+		if (ent:Health() < 1) then
+			ent:TakeDamage(50, ply, self)
+		end
+		
+	elseif (!IsValid(ent)) then
+		sound.Play(SwingSound, (ply:GetPos()))
+	
+	self:SetNextPrimaryFire(CurTime() + 1)
+	end
+
+		
+	
+	ply:LagCompensation(false)
+end
+
+function SWEP:CanSecondaryAttack()
+	return false
 end
