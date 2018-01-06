8f1ec37c094972ebf3867f3d181a800760a22a5e
Imacc2009 <josephfra@btinternet.com>
2018-01-06 02:57:44 +0000

    Add base teams and StartRound function
    Added the citizen, robot and parasyte teams and created a function to
    set players' teams when the round starts.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,2 +1,30 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
+
+function GM:StartRound()
+
+	local parasyteindex = math.random(#player.GetAll())
+	local parasyte = player.GetAll()[parasyteindex]
+	
+	local robotindex = math.random(#player.GetAll())
+	
+	while parasyteindex == robotindex do
+	
+		local robotindex = math.random(#player.GetAll())
+
+	end
+	
+	local robot = player.GetAll()[robotindex]
+	
+	parasyte:SetTeam(3)
+	robot:SetTeam(2)
+	
+	for k, v in pairs(player.GetAll()) do
+		if k == parasyteindex or k == robotindex then
+			continue
+		else
+			v:SetTeam(1)
+		end
+	end
+	
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,3 +2,11 @@ GM.Name = "Parasyte 13"
 GM.Author = "Hocka & josephfra"
 GM.Email = "N/A"
 GM.Website = "N/A"
+
+function GM:CreateTeams()
+
+	team.SetUp( 1, "Citizens", Color(0,200,0))
+	team.SetUp( 2, "Robot", Color(100,100,100))
+	team.SetUp( 3, "Parasyte", Color(200,0,0))
+
+end
